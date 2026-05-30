591be5a75678150e6080361f234b38f1b843754f
Anurag Gupta <anurag70508@gmail.com>
2026-05-31 02:40:49 +0530

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 ## End to End Data Science Project
+
+import dagshub
+dagshub.init(repo_owner='anurag70508', repo_name='mlproject', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
+
+MLFLOW_TRACKING_URI=https://dagshub.com/anurag70508/mlproject.mlflow \
+MLFLOW_TRACKING_PASSWORD=fd77f238608e3b03f1f9982d47fd75bcd2d95ec7 \
+MLFLOW_TRACKING_USERNAME=anurag70508 \
--- src/mlprojecct/components/model_trainer.py
+++ src/mlprojecct/components/model_trainer.py
@@ -1,11 +1,11 @@
 import os
 import sys
 from dataclasses import dataclass
-#from urllib.parse import urlparse
-#import mlflow
-#import mlflow.sklearn
-#import numpy as np
-#from sklearn.metrics import mean_squared_error,mean_absolute_error
+from urllib.parse import urlparse
+import mlflow
+import mlflow.sklearn
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -31,11 +31,11 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
 
-    # def eval_metrics(self,actual, pred):
-    #     rmse = np.sqrt(mean_squared_error(actual, pred))
-    #     mae = mean_absolute_error(actual, pred)
-    #     r2 = r2_score(actual, pred)
-    #     return rmse, mae, r2
+    def eval_metrics(self,actual, pred):
+        rmse = np.sqrt(mean_squared_error(actual, pred))
+        mae = mean_absolute_error(actual, pred)
+        r2 = r2_score(actual, pred)
+        return rmse, mae, r2
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
@@ -104,35 +104,35 @@ class ModelTrainer:
             ]
             best_model = models[best_model_name]
 
-            # print("This is the best model:")
-            # print(best_model_name)
+            print("This is the best model:")
+            print(best_model_name)
 
-            # model_names = list(params.keys())
+            model_names = list(params.keys())
 
-            # actual_model=""
+            actual_model=""
 
-            # for model in model_names:
-            #     if best_model_name == model:
-            #         actual_model = actual_model + model
+            for model in model_names:
+                if best_model_name == model:
+                    actual_model = actual_model + model
 
-            # best_params = params[actual_model]
+            best_params = params[actual_model]
 
-            # mlflow.set_registry_uri("https://dagshub.com/krishnaik06/mlprojecthindi.mlflow")
-            # tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            mlflow.set_registry_uri("https://dagshub.com/anurag70508/mlproject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-            # # mlflow
+            # mlflow
 
-            # with mlflow.start_run():
+            with mlflow.start_run():
 
-            #     predicted_qualities = best_model.predict(X_test)
+                predicted_qualities = best_model.predict(X_test)
 
-            #     (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
 
-            #     mlflow.log_params(best_params)
+                mlflow.log_params(best_params)
 
-            #     mlflow.log_metric("rmse", rmse)
-            #     mlflow.log_metric("r2", r2)
-            #     mlflow.log_metric("mae", mae)
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
 
 
             #     # Model registry does not work with file store
